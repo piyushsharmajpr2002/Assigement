6001814a5346fa2670cce4d0d3bdababfb5d73be
Piyush sharma <102236187+piyushsharmajpr2002@users.noreply.github.com>
2022-03-29 15:00:23 +0530

    Add files via upload

---
+++ seven_7_reverse_string.py
@@ -0,0 +1,6 @@
+def reverse_function(str):
+    """that is do reverse the string"""
+    return str[::-1]
+
+user_string=input("enter a string ")
+print("the reverse string is -:",reverse_function(user_string))
